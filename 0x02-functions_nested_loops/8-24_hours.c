8e5a8356ca86b7a77ecd06e20e830f55f578b880
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-02-21 22:14:01 +0300

    Hours and minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *jack_bauer- Entry point
+ *Description: Prints every minute os the day starting from 00:00 to 23:59
+ *Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 1; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
