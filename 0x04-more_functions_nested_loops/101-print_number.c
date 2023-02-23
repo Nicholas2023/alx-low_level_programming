f244779422d3e1a557d9c08c8212038c76cc9972
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-02-23 23:14:24 +0300

    Correction Task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
- * print_number - Entry point
- * Description: Prints an integer
- * @n: Carrier integer variable
- * Return: void
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
 
 void print_number(int n)
 {
+	unsigned int x;
+
+	x = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		x = -n;
 	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
