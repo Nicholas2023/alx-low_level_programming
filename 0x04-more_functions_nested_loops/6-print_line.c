83721347268db1a6f06cf92e84b74f35fbb82f2d
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-02-23 18:53:11 +0300

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description: Draws a straight line in the terminal
+ * @n: Carrier integer variable
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
