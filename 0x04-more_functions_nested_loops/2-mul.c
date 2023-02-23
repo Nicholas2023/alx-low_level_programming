ff0b867ef42e60220ced1137bd518b2b5248148a
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-02-23 17:28:48 +0300

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description: Multiplies 2 integers
+ * @a: Carrier variable
+ * @b: Carrier variable
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
