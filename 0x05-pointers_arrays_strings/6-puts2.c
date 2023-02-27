bc6fab18569fc4f99d2910b843ec06ac96bb57af
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-02-27 23:38:08 +0300

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * Description:  prints every other character of a string
+ * starting with the first character
+ * @str: String to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
