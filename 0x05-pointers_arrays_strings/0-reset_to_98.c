6226cc175fed19ef0f56e94233dd7294d28c0d95
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-02-27 15:43:42 +0300

    Initial coomit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Description: Takes a pointer to an int and updates its value
+ * @n: Integer address to be updated
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+##0x05. C - Pointers, arrays and strings
+
+Tackled tasks related to Pointers, arrays and strings
