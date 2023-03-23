ca10d0f2f4d65012062e38b4826c46ae7f3fc8d4
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-03-23 16:44:49 +0300

    Task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/**
- * Auth: Nicholas
- * Desc: A file containing the prototype of all functions
- * used to solve tasks on Variadic functions
- *
+/*
+ * File: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-variadic_functions directory.
  */
-
 
 #include <stdarg.h>
 
@@ -17,14 +15,13 @@
  * @print: A function pointer to a function that prints
  *         a data type corresponding to symbol.
  */
-
 typedef struct printer
 {
 	char *symbol;
-	void (*print)(va_list args);
+	void (*print)(va_list arg);
+
 } printer_t;
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
