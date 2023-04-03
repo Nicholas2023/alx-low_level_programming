374ec93c4ed8789fd9126b57cdbf2efae035a29d
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-04-04 00:42:11 +0300

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,11 +8,11 @@
  * Return: The node to be returned an printed
  */
 
-listint_t *get_nodeint_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int node_ind;
 
-	for (i = 0; i < index; i++)
+	for (node_ind = 0; node_ind < index; node_ind++)
 	{
 		if (head == NULL)
 			return (NULL);
