0e46a82ff1bfc29b4a17b1899ef2e51ac3f8e1eb
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-04-04 02:12:32 +0300

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 0; i < index; i++)
+	for (i = 1; i < index; i++)
 	{
 		if (dup->next == NULL)
 			return (-1);
