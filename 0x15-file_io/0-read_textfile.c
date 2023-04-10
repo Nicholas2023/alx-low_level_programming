70fc31bf3c57fa4d4060532e2c2109cfea80e6c0
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-04-10 16:02:59 +0300

    Initial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,44 +6,34 @@
  * @filename: Name of the file to be read and printed
  * @letters: The number of letters to be read and printed
  *
- * Return: 0 if file cannot be opened
- * 0 if filename is NULL
- * 0 if write fails
- * Actual number of letters to be read and printed
+ * Return: Actual number of letters to be read and printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/*Variable declaration*/
 
 	ssize_t f_open, f_read, f_write;
 	char *tmp;
 
 	/*Check if filename is NULL*/
-
 	if (filename == NULL)
 		return (0);
 
 	/*Memory allocation for read and written letters*/
-
 	tmp = malloc(sizeof(char) * letters);
 	if (tmp == NULL)
 		return (0);
 
 	/*File opening and mode stating*/
-
 	f_open = open(filename, O_RDONLY);
 
 	/*File reading*/
-
 	f_read = read(f_open, tmp, letters);
 
 	/*File writing to stdout*/
-
 	f_write = write(STDOUT_FILENO, tmp, f_read);
 
 	/*Check for errors in system calls*/
-
 	if (f_open == -1 || f_read == -1 || f_write == -1 || f_write != f_read)
 	{
 		free(tmp);
@@ -51,14 +41,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	/*Free the dynamic memory, previously allocated*/
-
 	free(tmp);
 
 	/*Close the file*/
-
 	close(f_open);
 
 	/*Return number of bytes written to stdout*/
-
 	return (f_write);
 }
