2bdd315125150de7d2aaba9bf60c0eba1c1d557a
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-22 17:47:08 +0300

    task100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,64 @@
+/**
+ * add - Adds two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: Sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Finds the difference of two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: Difference of two integers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: Product of two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: Quotient of two integers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Finds the modulus of two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: Modulus of two integers
+ */
+
+int mod(int a, int b)
+{	
+	return (a % b);
+}
