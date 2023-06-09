58a26e766bcbbafdfbbe4d80723d57ffbc865ce8
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-06-09 13:53:21 +0300

    task1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+/**
+ * hash_djb2 - Calculates the DJB2 hash value
+ * for a given string
+ * @str: The input to be hashed
+ *
+ * Return: The calculated hash value
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
