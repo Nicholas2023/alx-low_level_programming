eb2b1a92801b33dfa1c66e6b576bf1c6ba4b6866
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-12 10:38:50 +0300

    task452

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -8,7 +8,8 @@
  * @list: Pointer to the head of the skip list to search in.
  * @value: The value to search for.
  *
- * Return: A pointer to the first node where value is located, or NULL if not found.
+ * Return: A pointer to the first node where value
+ * is located, or NULL if not found.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
@@ -28,7 +29,8 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	if (express->n >= value)
 	{
-		printf("Value found between indexes [%lu] and [%lu]\n", prev->index, express->index);
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				prev->index, express->index);
 		express = prev;
 	}
 
