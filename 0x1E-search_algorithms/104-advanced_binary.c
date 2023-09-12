32ec2e4cf8921c6d801a0157d18a1ab40c890de0
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-12 09:36:45 +0300

    task104

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -12,35 +12,35 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t middle = size / 2, i;
-    int result;
+	size_t middle = size / 2, i;
+	int result;
 
-    if (array == NULL || size == 0)
-        return (-1);
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
+	if (array == NULL || size == 0)
+		return (-1);
 
-    if (array[middle] == value)
-    {
-        if (middle == 0 || array[middle - 1] < value)
-            return (middle);
-    }
-    if (array[middle] >= value)
-        return (advanced_binary(array, middle, value));
-    else
-    {
-        result = advanced_binary(array + middle + 1, size - middle - 1, value);
-        if (result == -1)
-            return (-1);
-        return (middle + 1 + result);
-    }
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
 
-    return (-1);
+	if (array[middle] == value)
+	{
+		if (middle == 0 || array[middle - 1] < value)
+			return (middle);
+	}
+	if (array[middle] >= value)
+		return (advanced_binary(array, middle, value));
+	{
+		result = advanced_binary(array + middle + 1, size - middle - 1, value);
+		if (result == -1)
+			return (-1);
+		return (middle + 1 + result);
+	}
+
+	return (-1);
 }
