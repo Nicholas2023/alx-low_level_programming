5db2a1ab227b50dbb5dbc4b0482c014a000e8f04
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-12 10:17:27 +0300

    task123

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stddef.h>
-#include <math.h>
 #include "search_algos.h"
 
 /**
@@ -8,38 +7,42 @@
  * @list: Pointer to the head of the skip list to search in.
  * @value: The value to search for.
  *
- * Return: A pointer to the first node where value
+ * Return: A pointer to the first node where the value
  * is located, or NULL if not found.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	if (list == NULL)
+	skiplist_t *current, *express;
+
+	if (!list)
 		return (NULL);
 
-	skiplist_t *express = list, *prev;
-
+	express = list;
 	while (express->express)
 	{
-		prev = express;
-		express = express->express;
 		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
 		if (express->n >= value)
 			break;
+		current = express;
+		express = express->express;
 	}
 
-	if (express->n >= value)
+	if (!express->express && express->n < value)
 	{
-		printf("Value found between indexes [%lu] and [%lu]\n",
-				prev->index, express->index);
-		express = prev;
+		current = express;
+		while (express->next)
+			express = express->next;
 	}
 
-	while (express)
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			current->index, express->index);
+
+	while (current)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-		if (express->n == value)
-			return (express);
-		express = express->next;
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		if (current->n == value)
+			return (current);
+		current = current->next;
 	}
 
 	return (NULL);
